faae808e1df700086e11fb72ac9d593e9218531a
Olaf_JS <61488698+Jeeseul@users.noreply.github.com>
2021-01-29 00:25:47 +0900

    Update AdminDAO.java

--- OAS/src/main/java/com/walab/oas/DAO/AdminDAO.java
+++ OAS/src/main/java/com/walab/oas/DAO/AdminDAO.java
@@ -1,5 +1,24 @@
 package com.walab.oas.DAO;
 
+import java.util.List;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.walab.oas.DTO.Field;
+
+
+@Repository
 public class AdminDAO {
 		
+	@Autowired
+	SqlSessionTemplate sqlSession;
+	
+	private static String namespace ="com.walab.oas.mappers.oas_mapper";
+
+	public List<Field> getFieldList(){//form_id를 가져와야하는 것 아닌가?
+		
+		return sqlSession.selectList(namespace+".getFieldList");
+			
+	}
 }
