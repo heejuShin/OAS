e8b8745a8925fc3e2ccd059239386db10de9901d
Olaf_JS <61488698+Jeeseul@users.noreply.github.com>
2021-03-29 19:05:51 +0900

    체크박스, 드롭다운, 객관식 선택지 엔터로 입력되도록

--- OAS/src/main/webapp/resources/assets/js/formCreate.js
+++ OAS/src/main/webapp/resources/assets/js/formCreate.js
@@ -297,7 +297,7 @@ $( document ).ready(function() {
 	  $(this).parent().remove();
 	})
 	
-	//객관식 아이템 추가
+	//객관식 아이템 추가-버튼 클릭시
 	$("#list").on('click', ".btn_add_radio", function(){
 	
 	  //문자열 처리 (, 기준)
@@ -327,6 +327,79 @@ $( document ).ready(function() {
 		$(this).siblings("input").val("");
 	});
 	
+	//객관식,체크박스,드롭다운 아이템 추가-엔터 입력시 
+	$("#list").on('keydown', ".inputs", function(e){
+		if (e.keyCode === 13) {
+		  	//문자열 처리 (, 기준)
+			var inputs = $(this).val();
+			var lastChar = inputs.charAt(inputs.length-1);
+			 
+	
+			if(lastChar == ','){
+				inputs = inputs.slice(0,-1); //마지막 문자 콤마(,) 지움 
+			}
+			
+			inputs = inputs.split(",");
+			
+			//객관식 
+			var s_o = $(this).parent(".content").siblings(".field_type").val();
+			
+			
+			if(s_o == "radio"){
+				for(var i = 0 ; i < inputs.length; i++){
+					if(inputs[i]=="") continue;
+					var r_cnt = parseInt($(this).parent().siblings(".count").val())+1;
+					  $(this).parent().siblings(".count").val(r_cnt);
+					  
+					  var idx = $(this).parent().siblings(".index").val();
+					  
+					  $("#radio_add").find(".radio_real").attr("name", idx+"content"+String(r_cnt));
+					  $("#radio_add").find(".radio_real").attr("value",inputs[i]);
+					  $("#radio_add").find("label").html(inputs[i]);
+					  $("#radio_add").find(".isItemOri").attr("name", idx+"isItemOri"+String(r_cnt));
+					  $(this).siblings(".list_radio").append($("#radio_add").html());
+					  
+				}
+			}
+			
+			//체크박스 
+			if(s_o == "checkbox"){
+				for(var i = 0 ; i < inputs.length; i++){
+	 			if(inputs[i]=="") continue;
+	 			var c_cnt = parseInt($(this).parent().siblings(".count").val())+1;
+	 	 		  $(this).parent().siblings(".count").val(c_cnt);
+	
+	 	  		  var idx = $(this).parent().siblings(".index").val();
+	
+	 			  $("#chxbox_add").find(".checkbox_real").attr("name", idx+"content"+String(c_cnt));
+	 			  $("#chxbox_add").find(".checkbox_real").attr("value",inputs[i]);
+	 			  $("#chxbox_add").find(".isItemOri").attr("name", idx+"isItemOri"+String(c_cnt));
+	 			  $("#chxbox_add").find("label").html(inputs[i]);
+	 			  $(this).siblings(".list_chxbox").append($("#chxbox_add").html());
+	 			}
+ 			}
+ 			//드롭다운
+ 			if(s_o == "select"){ 
+	 			for(var i = 0 ; i < inputs.length; i++){
+		 			if(inputs[i]=="") continue;
+		 	  		var o_cnt = parseInt($(this).parent().siblings(".count").val())+1;
+		 	 		 $(this).parent().siblings(".count").val(o_cnt);
+		
+		 	  		 var idx = $(this).parent().siblings(".index").val();
+		
+		 			  $("#select_value_add").find(".option_real").attr("name", idx+"content"+String(o_cnt));
+		 			  $("#select_value_add").find(".option_real").attr("value",inputs[i]);
+		 			  $("#select_add").find("option").attr("value", inputs[i]);
+		 			  $("#select_add").find("option").html(inputs[i]);
+		 			  $(this).siblings("select").append($("#select_add").html());
+		 			  $(this).siblings(".list_select").append($("#select_value_add").html());
+		 	  	}
+			}
+			$(this).val("");
+			
+		}
+	});
+	
 	//아이템 삭제
 	$("#list").on('click', ".remove_item_Create", function(){
 	  $(this).parent().remove();
@@ -346,7 +419,7 @@ $( document ).ready(function() {
 	
 	  $(this).parent().remove();
 	});
-	//체크박스 아이템 추가
+	//체크박스 아이템 추가-버튼 클릭시
 	$("#list").on('click', ".btn_add_chxbox", function(){
 
  	  //문자열 처리 (, 기준)
@@ -374,7 +447,7 @@ $( document ).ready(function() {
  		}
  		$(this).siblings("input").val("");
  	});
- 	//드롭다운 아이템 추가
+ 	//드롭다운 아이템 추가 - 버튼 
  	$("#list").on('click', ".btn_add_select", function(){
 
  	  //문자열 처리 (, 기준)
